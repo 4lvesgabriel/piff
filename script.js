7f92f762191198402ecfb8751f7b1aaf4d67046d
Gabriel Alves Gabriel <g.alvesgabriel27@gmail.com>
2025-12-04 22:37:49 -0300

    alteracoes front end base

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 $(document).ready(() => {
-    $("#piff2").addClass('dnone');
+        $('.textbox').hide();
+        $('#piff2').hide();
 
     setTimeout(function() {
         $('#piff1').hide();
         $('#piff2').show();
     }, 5000);
+
+    setTimeout(function() {
+        $('#piff2').addClass("blur");
+        $('.textbox').show();
+    }, 6000);
 });
